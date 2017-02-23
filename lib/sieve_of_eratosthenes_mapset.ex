f6bb2ff50090f832f88355c2b16bb5e7e21c9e2b
yuriibodarev <yuriibodarev@gmail.com>
2017-02-23 13:19:25 +0200

    MapSet: code refactoring

--- lib/sieve_of_eratosthenes_mapset.ex
+++ lib/sieve_of_eratosthenes_mapset.ex
@@ -20,26 +20,25 @@ defmodule Primes.SieveOfEratosthenes.MapSet do
   def get_primes_list(limit) when limit == 2, do: [2]
   
   def get_primes_list(limit) when limit > 2 do
-    odd_integers = Enum.filter(3..limit, &is_odd/1)
+    odd_integers = get_odd_integers(3, limit)
     integers_set = MapSet.new([2 | odd_integers])
 
     sieve(integers_set, 3, limit)
   end
 
-  # Sieving
-  # upper limit reached, get all primes that are left in the set as list
-  defp sieve(integers_set, next_int, limit) when next_int * next_int > limit do
+  # Sieving: limit reached, get all numbers that are left in the set - this numbers is primes
+  defp sieve(integers_set, next, limit) when next * next > limit do
     MapSet.to_list(integers_set)
     |> Enum.sort()
   end
 
- # iterative algorithm step: remove all numbers that are composites to the current 
- # integer if the current integer is present in set
-  defp sieve(integers_set, next_int, limit) do
-    updated_integers_set =
-      if MapSet.member?(integers_set, next_int) do
+  # Sieving: check if the next prime candidate integer is in set,
+  # if it is - than remove all composites for this prime
+  defp sieve(integers_set, next, limit) do
+    new_integers_set =
+      if MapSet.member?(integers_set, next) do
         composites = 
-          get_composites(next_int * next_int, 2 * next_int, limit)
+          get_composites(next * next, 2 * next, limit)
           |> MapSet.new()
         
         MapSet.difference(integers_set, composites)
@@ -47,10 +46,10 @@ defmodule Primes.SieveOfEratosthenes.MapSet do
         integers_set
       end
 
-    sieve(updated_integers_set, next_int + 1, limit)
+    sieve(new_integers_set, next + 1, limit)
   end
 
-  defp get_composites(number, step, limit) do
-    :lists.seq(number, limit, step)
-  end
+  defp get_odd_integers(first, limit) when is_odd(first), do: :lists.seq(first, limit, 2)
+  
+  defp get_composites(number, step, limit), do: :lists.seq(number, limit, step)
 end
